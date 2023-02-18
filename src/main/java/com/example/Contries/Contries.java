376e5c89547d2e5b37431d92357de70a611648bb
Jan Jeżewski <Jeewski533@gmail.com>
2023-02-18 03:01:23 +0100

    first commit

---
+++ src/main/java/com/example/Contries/Contries.java
@@ -0,0 +1,50 @@
+package com.example.Contries;
+
+public class Contries {
+
+    private int id;
+    private String name;
+    private int population;
+    private String language;
+    private String currency;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+}
